7df3991b9f0b41f7fbddd33f01e1dc223cb0681e
Wren Durbano <wren.durbano@gmail.com>
2025-04-20 19:17:45 -0600

    add build script

---
+++ build.bash
@@ -0,0 +1,6 @@
+version=v0.1
+deno compile --allow-read --allow-write --target x86_64-pc-windows-msvc --output dist/clocktower-print-$version-windows-x86 ./src/main.ts
+deno compile --allow-read --allow-write --target x86_64-apple-darwin --output dist/clocktower-print-$version-macos-x86 ./src/main.ts
+deno compile --allow-read --allow-write --target aarch64-apple-darwin --output dist/clocktower-print-$version-macos-arm ./src/main.ts
+deno compile --allow-read --allow-write --target x86_64-unknown-linux-gnu --output dist/clocktower-print-$version-linux-x86 ./src/main.ts
+deno compile --allow-read --allow-write --target aarch64-unknown-linux-gnu --output dist/clocktower-print-$version-linux-arm ./src/main.ts
